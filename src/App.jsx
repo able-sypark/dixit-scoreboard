81b0ee293895face7253a3ac95132c8c08222a55
sypark <sy.park@ablelabsinc.com>
2024-12-30 00:01:58 +0900

    add: html, css with grid

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,95 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="container">
+      <div className="column-header">Player</div>
+      <div className="item header">john</div>
+      <div className="item header">loy</div>
+      <div className="item header">paul</div>
+      <div className="item header">sam</div>
+      <button>Edit</button>
+      <div className="column-header">Total Score</div>
+      <div className="item header">16</div>
+      <div className="item header">10</div>
+      <div className="item header">2</div>
+      <div className="item header">5</div>
+      <div></div>
+      <div className="section-splitter" />
+      <div className="column-header">Story teller</div>
+      <input className="item" type="checkbox" checked={true} />
+      <input className="item" type="checkbox" />
+      <input className="item" type="checkbox" />
+      <input className="item" type="checkbox" />
+      <div className="column-header">Guess</div>
+      <div className="item">-</div>
+      <input className="item" type="checkbox" />
+      <input className="item" type="checkbox" />
+      <input className="item" type="checkbox" />
+      <div className="column-header">Votes</div>
+      <div className="item">-</div>
+      <div className="item number-counter">
+        <button>▲</button>
+        <div>1</div>
+        <button>▼</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="item number-counter">
+        <button>▲</button>
+        <div>1</div>
+        <button>▼</button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      <div className="item number-counter">
+        <button>▲</button>
+        <div>0</div>
+        <button>▼</button>
+      </div>
+      <div className="column-header">Round Score</div>
+      <div className="item">3</div>
+      <div className="item">4</div>
+      <div className="item">1</div>
+      <div className="item">0</div>
+      <button style={{ gridColumn: 6, gridRowStart: 4, gridRowEnd: 8 }}>
+        Submit
+      </button>
+      <div className="section-splitter" />
+      <div className="column-header">Round 4</div>
+      <div className="item">4</div>
+      <div className="item">2</div>
+      <div className="item">2</div>
+      <div className="item">0</div>
+      <div className="history-action">
+        <button>Edit</button>
+        <button>Delete</button>
+      </div>
+      <div className="column-header">Round 3</div>
+      <div className="item">#</div>
+      <div className="item">#</div>
+      <div className="item">#</div>
+      <div className="item">#</div>
+      <div className="history-action">
+        <button>Edit</button>
+        <button>Delete</button>
+      </div>
+      <div className="column-header">Round 2</div>
+      <div className="item">#</div>
+      <div className="item">#</div>
+      <div className="item">#</div>
+      <div className="item">#</div>
+      <div className="history-action">
+        <button>Edit</button>
+        <button>Delete</button>
+      </div>
+      <div className="column-header">Round 1</div>
+      <div className="item">#</div>
+      <div className="item">#</div>
+      <div className="item">#</div>
+      <div className="item">#</div>
+      <div className="history-action">
+        <button>Edit</button>
+        <button>Delete</button>
+      </div>
+    </div>
+  );
+};
 
-export default App
+export default App;
