1d0e9ff85ac1e00bd75e403e4b1b4004dfffb213
sypark <sy.park@ablelabsinc.com>
2025-01-04 11:01:31 +0900

    add: state

--- src/App.jsx
+++ src/App.jsx
@@ -1,95 +1,168 @@
+import { useState } from "react";
 import "./App.css";
 
-const App = () => {
+function App() {
+  const [players, setPlayers] = useState([
+    { id: 1, name: "john" },
+    { id: 2, name: "loy" },
+    { id: 3, name: "paul" },
+    { id: 4, name: "sam" },
+  ]);
+
+  const [roundHistorys, setRoundHistorys] = useState([
+    {
+      roundId: 1,
+      storytellerId: 1,
+      guessIds: [2, 3],
+      votes: {
+        1: null, // john
+        2: 1, // loy
+        3: 0, // paul
+        4: 0, // sam
+      },
+      scores: {
+        1: 3, // john
+        2: 4, // loy
+        3: 3, // paul
+        4: 0, // sam
+      },
+    },
+    {
+      roundId: 2,
+      storytellerId: 2,
+      guessIds: [],
+      votes: {
+        1: 0,
+        2: null,
+        3: 1,
+        4: 2,
+      },
+      scores: {
+        1: 2,
+        2: 0,
+        3: 3,
+        4: 4,
+      },
+    },
+    // 다른 라운드 객체들 추가...
+  ]);
+
+  const [roundInput, setRoundInput] = useState({
+    isStorytellers: [false, false, true, false],
+    isGuesses: [true, false, null, false],
+    votes: [2, 0, null, 0],
+    scores: [5, 0, 3, 0],
+  });
+
+  const playersOrder = players.map((player) => player.id);
+  const roundIdScores = roundHistorys
+    .sort((a, b) => b.roundId - a.roundId) // roundId 기준 내림차순 정렬
+    .map((roundInput) => {
+      // 각 roundInput에 대해 scores를 playerId 순으로 정렬
+      const sortedScores = playersOrder.map(
+        (playerId) => roundInput.scores[playerId]
+      );
+      return {
+        roundId: roundInput.roundId,
+        scores: sortedScores,
+      };
+    });
+  const totalScores = playersOrder.map((_, index) =>
+    roundIdScores.reduce((sum, round) => sum + round.scores[index], 0)
+  );
+
   return (
     <div className="container">
       <div className="column-header">Player</div>
-      <div className="item header">john</div>
-      <div className="item header">loy</div>
-      <div className="item header">paul</div>
-      <div className="item header">sam</div>
+      {players.map((player) => (
+        <div key={player.id} className="item header">
+          {player.name}
+        </div>
+      ))}
       <button>Edit</button>
+
       <div className="column-header">Total Score</div>
-      <div className="item header">16</div>
-      <div className="item header">10</div>
-      <div className="item header">2</div>
-      <div className="item header">5</div>
+      {totalScores.map((totalScore, index) => (
+        <div key={index} className="item header">
+          {totalScore}
+        </div>
+      ))}
       <div></div>
+
       <div className="section-splitter" />
+
       <div className="column-header">Story teller</div>
-      <input className="item" type="checkbox" checked={true} />
-      <input className="item" type="checkbox" />
-      <input className="item" type="checkbox" />
-      <input className="item" type="checkbox" />
+      {roundInput.isStorytellers.map((isStoryteller, index) => (
+        <input
+          key={index}
+          className="item"
+          type="checkbox"
+          checked={isStoryteller}
+        />
+      ))}
+
       <div className="column-header">Guess</div>
-      <div className="item">-</div>
-      <input className="item" type="checkbox" />
-      <input className="item" type="checkbox" />
-      <input className="item" type="checkbox" />
+      {roundInput.isGuesses.map((isGuess, index) =>
+        isGuess === null ? (
+          <div key={index} className="item">
+            -
+          </div>
+        ) : (
+          <input
+            key={index}
+            className="item"
+            type="checkbox"
+            checked={isGuess}
+          />
+        )
+      )}
+
       <div className="column-header">Votes</div>
-      <div className="item">-</div>
-      <div className="item number-counter">
-        <button>▲</button>
-        <div>1</div>
-        <button>▼</button>
-      </div>
-      <div className="item number-counter">
-        <button>▲</button>
-        <div>1</div>
-        <button>▼</button>
-      </div>
-      <div className="item number-counter">
-        <button>▲</button>
-        <div>0</div>
-        <button>▼</button>
-      </div>
+      {roundInput.votes.map((vote, index) =>
+        vote === null ? (
+          <div key={index} className="item">
+            -
+          </div>
+        ) : (
+          <div key={index} className="item number-counter">
+            <button>▲</button>
+            <div>{vote}</div>
+            <button>▼</button>
+          </div>
+        )
+      )}
+
       <div className="column-header">Round Score</div>
-      <div className="item">3</div>
-      <div className="item">4</div>
-      <div className="item">1</div>
-      <div className="item">0</div>
+      {roundInput.scores.map((score, index) => (
+        <div key={index} className="item">
+          {score}
+        </div>
+      ))}
+
       <button style={{ gridColumn: 6, gridRowStart: 4, gridRowEnd: 8 }}>
         Submit
       </button>
+
       <div className="section-splitter" />
-      <div className="column-header">Round 4</div>
-      <div className="item">4</div>
-      <div className="item">2</div>
-      <div className="item">2</div>
-      <div className="item">0</div>
-      <div className="history-action">
-        <button>Edit</button>
-        <button>Delete</button>
-      </div>
-      <div className="column-header">Round 3</div>
-      <div className="item">#</div>
-      <div className="item">#</div>
-      <div className="item">#</div>
-      <div className="item">#</div>
-      <div className="history-action">
-        <button>Edit</button>
-        <button>Delete</button>
-      </div>
-      <div className="column-header">Round 2</div>
-      <div className="item">#</div>
-      <div className="item">#</div>
-      <div className="item">#</div>
-      <div className="item">#</div>
-      <div className="history-action">
-        <button>Edit</button>
-        <button>Delete</button>
-      </div>
-      <div className="column-header">Round 1</div>
-      <div className="item">#</div>
-      <div className="item">#</div>
-      <div className="item">#</div>
-      <div className="item">#</div>
-      <div className="history-action">
-        <button>Edit</button>
-        <button>Delete</button>
-      </div>
+
+      {roundIdScores.map((roundScore) => {
+        return (
+          <>
+            <div className="column-header">Round {roundScore.roundId}</div>
+            {roundScore.scores.map((score, index) => (
+              <div key={index} className="item">
+                {score}
+              </div>
+            ))}
+            <div className="history-action">
+              <button>Edit</button>
+              <button>Delete</button>
+            </div>
+          </>
+        );
+      })}
     </div>
   );
-};
+}
 
 export default App;
